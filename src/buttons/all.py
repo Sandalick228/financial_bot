230d36562954a7d8e99e4e0177ba6adce9da7e68
Sandalick228 <dbnxn15@gmail.com>
2026-02-14 12:22:13 +0700

    FIN-01:bcghfdbk bcghfdbk исправил инлайн клавиатуру(почему-то не удаляется тесла (текст) который я пишу тут)

--- src/buttons/all.py
+++ src/buttons/all.py
@@ -2,11 +2,19 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 main_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-    [InlineKeyboardButton(text='Пополнение', callback_data='current_tasks'),
-    InlineKeyboardButton(text='Расход', callback_data='completed_tasks')],
-    [InlineKeyboardButton(text='Мои счета', callback_data='mark_completed_tasks'),
-    InlineKeyboardButton(text='Статистика', callback_data='add_task')],
-    [InlineKeyboardButton(text='Настройки', callback_data='delete_task'),
-     InlineKeyboardButton(text='Премиум', callback_data='delete_task')]
+    [InlineKeyboardButton(text='Пополнение', callback_data='depositing'),
+    InlineKeyboardButton(text='Расход', callback_data='expense')],
+    [InlineKeyboardButton(text='Мои счета', callback_data='my_accounts'),
+    InlineKeyboardButton(text='Статистика', callback_data='statistics')]
     ]
+)
+
+back_to_main = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text='назад', callback_data='back_to_main')]
+    ]
+)
+
+add_account = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text='Добавить счёт', callback_data='add_an_invoice')]]
 )
